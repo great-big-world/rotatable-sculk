98644c2824eae7678dce457c29d9273f4239dcfc
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-03-27 10:22:14 -0400

    Fix redirect

--- src/main/java/dev/creoii/rotatablesculk/mixin/client/WorldEventHandlerMixin.java
+++ src/main/java/dev/creoii/rotatablesculk/mixin/client/WorldEventHandlerMixin.java
@@ -1,5 +1,7 @@
 package dev.creoii.rotatablesculk.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import dev.creoii.rotatablesculk.util.ExtendedShriekParticleEffect;
 import dev.creoii.rotatablesculk.util.SculkRotationHelper;
@@ -13,19 +15,18 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LevelEventHandler.class)
 public class WorldEventHandlerMixin {
     @Shadow @Final private ClientLevel level;
 
-    @Redirect(method = "levelEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V", ordinal = 9))
-    private void gbw$fixSculkShriekParticleOffset(ClientLevel instance, ParticleOptions parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ, @Local(argsOnly = true) BlockPos pos) {
+    @WrapOperation(method = "levelEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V", ordinal = 9))
+    private void gbw$fixSculkShriekParticleOffset(ClientLevel instance, ParticleOptions particleOptions, double d, double e, double f, double g, double h, double i, Operation<Void> original, @Local(argsOnly = true) BlockPos pos) {
         BlockState state = level.getBlockState(pos);
         double[] offsets = SculkRotationHelper.getShriekParticleOffsets(state);
-        if (parameters instanceof ExtendedShriekParticleEffect extendedShriekParticleEffect && state.hasProperty(BlockStateProperties.FACING)) {
+        if (particleOptions instanceof ExtendedShriekParticleEffect extendedShriekParticleEffect && state.hasProperty(BlockStateProperties.FACING)) {
             extendedShriekParticleEffect.gbw$setDirection(state.getValue(BlockStateProperties.FACING));
         }
-        instance.addParticle(parameters, pos.getX() + offsets[0], pos.getY() + offsets[1], pos.getZ() + offsets[2], 0f, 0f, 0f);
+        instance.addParticle(particleOptions, pos.getX() + offsets[0], pos.getY() + offsets[1], pos.getZ() + offsets[2], 0f, 0f, 0f);
     }
 }
